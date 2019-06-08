e30f8cf4cc276872dc9c84d5f1dcc0e07be86ee3
Dilanjan Opatha <dilanjano@gmail.com>
2019-06-08 18:33:14 +0530

    First github commit

---
+++ registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dilanjan
+ * Date: 6/8/19
+ * Time: 3:43 PM
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Dilanjan_CustomCatalog',
+    __DIR__
+);
